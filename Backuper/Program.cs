510c3f96e69fcf8febb81a6d82c592a06ed68fe3
NikitaG <nrg1@tpu.ru>
2022-08-27 16:07:51 +0700

    FIrst commit

---
+++ Backuper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Backuper
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
